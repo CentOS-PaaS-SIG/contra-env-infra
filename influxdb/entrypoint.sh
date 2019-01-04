724121f9add5e50a03984d111ddc49ece2cfb083
jstuart <jstuart@redhat.com>
2019-01-04 16:03:09 -0600

    add uid entrypoint to influxdb

--- influxdb/entrypoint.sh
+++ influxdb/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 
 if [ "${1:0:1}" = '-' ]; then
     set -- influxd "$@"
