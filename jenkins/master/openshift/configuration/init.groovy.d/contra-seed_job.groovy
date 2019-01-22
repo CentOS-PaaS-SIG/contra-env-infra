0a5f6f8dbfb588d7cdbe80aeb6cd2ad63c2c0fa3
jstuart <jstuart@redhat.com>
2019-01-22 16:11:03 -0600

    check if var is defined

--- jenkins/master/openshift/configuration/init.groovy.d/contra-seed_job.groovy
+++ jenkins/master/openshift/configuration/init.groovy.d/contra-seed_job.groovy
@@ -12,15 +12,18 @@ import java.util.logging.Logger
 
 def logger = Logger.getLogger("")
 env = System.getenv()
-if (env['LOAD_SEED_JOB'] == 'true') {
-    logger.info('Disabling job dsl script security')
-    GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).useScriptSecurity=false
-    def JENKINS_SEED_JOB = env['JENKINS_SEED_JOB'] ?: "${env['JENKINS_HOME']}/seed_job.dsl"
-    def config = new File(JENKINS_SEED_JOB).text
+if (env['LOAD_SEED_JOB']) {
+    if (env['LOAD_SEED_JOB'].toBoolean()) {
+        logger.info('Disabling job dsl script security')
+        GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).useScriptSecurity=false
+        def JENKINS_SEED_JOB = env['JENKINS_SEED_JOB'] ?: "${env['JENKINS_HOME']}/seed_job.dsl"
+        def config = new File(JENKINS_SEED_JOB).text
 
-    def workspace = new File("${env['JENKINS_HOME']}")
+        def workspace = new File("${env['JENKINS_HOME']}")
 
-    def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
-    new DslScriptLoader(jobManagement).runScript(config)
-    logger.info('Created seed job')
+        def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
+        new DslScriptLoader(jobManagement).runScript(config)
+        logger.info('Created seed job')
+    }
 }
+
